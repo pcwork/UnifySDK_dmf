c29a0e3d6dddf22d4ce9cbe920f0df2a7d7ab7f3
songhao wang <601465430@qq.com>
2026-06-18 13:58:21 +0800

    Add ZigPC discovery interview component
    Port the v1.6 discovery engine into the lean v1.7 ZigPC tree so gateway discovery callbacks can drive endpoint and cluster interview state in the datastore. This adds the discovery process, timeout handling, callback fan-out, and unit-test scaffolding, and wires the fixture into ZigPC startup without pulling in the older controller stack.
    
    Signed-off-by: songhao wang <songhao.wang_1@signify.com>

--- applications/zigpc/main.c
+++ applications/zigpc/main.c
@@ -14,6 +14,7 @@
 #include "zigpc_config.h"
 #include "zigpc_config_fixt.h"
 #include "zigpc_datastore_fixt.h"
+#include "zigpc_discovery.h"
 #include "zigpc_gateway.h"
 #include "zigpc_gateway_process.h"
 
@@ -38,13 +39,15 @@ static uic_fixt_setup_step_t uic_fixt_setup_steps_list[]
   {&zigpc_datastore_fixt_setup, "ZigPC Datastore"},
   {&attribute_store_init, "Attribute store"},
   {&zigpc_gateway_process_setup, "ZigPC Gateway"},
+  {&zigpc_discovery_fixt_setup, "ZigPC Discovery"},
   {&zigpc_gateway_network_init_fixt_setup, "ZigPC Network Init"},
   {&unify_dotdot_attribute_store_init, "Unify DotDot Attribute Store"},
   {&uic_mqtt_dotdot_init, "DotDot MQTT"},
      {NULL, "Terminator"}};
 
 static uic_fixt_shutdown_step_t uic_fixt_shutdown_steps_list[]
-  = {{&zigpc_gateway_process_shutdown, "ZigPC Gateway"},
+  = {{&zigpc_discovery_fixt_shutdown, "ZigPC Discovery"},
+     {&zigpc_gateway_process_shutdown, "ZigPC Gateway"},
      {&attribute_store_teardown, "Attribute store"},
      {&datastore_fixt_teardown, "Datastore"},
      {NULL, "Terminator"}};
